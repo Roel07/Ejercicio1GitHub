2b42135ad5bf4fb606497fda1fae2c3b0e647fe4
Roel García <brandonescobal201602762.edu.gt>
2021-06-10 13:11:56 -0600

    Ejercicio 2 en java

---
+++ Ejercicio2GitHub/Ejercicio2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Ejercicio2{
+
+
+
+public static void main(String[] args) {
+	
+	Scanner entrada = new Scanner(System.in);
+	int n;
+
+	System.out.println("Ingrese un numero entero");
+	n = entrada.nextInt();
+
+	for (int i=n; i > 0  ; i-- ) {
+
+
+		System.out.println("  " + i);
+	}
+
+
+
+}
+}	
